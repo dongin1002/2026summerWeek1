64facb7081eb33192ac131952e9c20d7006ce081
dongin1002 <155039243+dongin1002@users.noreply.github.com>
2026-07-05 18:24:13 +0900

    Refactor loops and remove commented code

--- main.cpp
+++ main.cpp
@@ -1,61 +1,57 @@
-
 #include <iostream>
 int main()
 {
-    /* int numOfStudents{ 30 };
-    numOfStudents = 22;
-    std::cout << numOfStudents << std::endl; //'l'ine
-    std::cout << sizeof(numOfStudents)<< std::endl;
-    std::cout << typeid(numOfStudents).name() << std::endl;
-    std::cout << typeid(static_cast<double>(numOfStudents)).name()<< std::endl; 
-
-    int n{2};
-    std::cout << 2 * n++ << std::endl;
-    std::cout << n << std::endl;
-
-    int m{2};
-    std::cout << 2 * ++m << std::endl;
-    std::cout << m << std::endl; 
-    
-    std::cout << "Enter two integers: ";
-    int x{}, y{}, z{};
-    std::cin >> x >> y;
-    
-    //if(x>y) z=x; else z= y;
-    z = z > y ? z : y;
-    std::cout << z << std::endl;
-    //if (x > 0) std::cout << "positive\n";
-    //else if (x < 0) std::cout << "negative\n";
-    //else 
-    //    std::cout << "zero\n"; 
-    std::cout << "Enter 1, 2, or 3: ";
-    int x{};
-    std::cin >> x;
-    switch(x)
+    int i{ 1 };
+    const int end{ 10 };
+    do 
     {
-        case 1: std::cout << "one\n"; break;
-        case 2: std::cout << "two\n"; break;
-        case 3: std::cout << "three\n"; break;
-        default: std::cout << "unknown\n";
-    } */
-    int i{1};
-    const int end{10};
-    do
-    {
-        std:: cout << i << ' '; ++i;
-    } while (i <= end);
+        std::cout << i << ' '; ++i;
+    } while(i <=  end);
     std::cout << std::endl;
 
     i = 1;
-    while (i<=end);
-    {
-        std:: cout << i << ' '; ++i;
-    } 
+    while ( i <= end ){
+        std::cout << i <<' '; ++i;
+    }
     std::cout << std::endl;
 
-    for (i=1;i<=end;++i)
-        std:: cout << i << ' ';
+    for (i = 1; i<=end; ++i)
+        std::cout << i << ' ';
     std::cout << std::endl;
+        
+    // std::cout << "Enter 1, 2, or 3: ";
+    // int x{};
+    // std::cin >> x;
+    // switch(x)
+    // {
+    //     case 1: std::cout << "one\n"; break;
+    //     case 2: std::cout << "two\n"; break;
+    //     case 3: std::cout << "three\n"; break;
+    //     default: std::cout << "unknown\n";
+    // }
+    // std::cout << "Enter two integers: ";
+    // int x{}, y{}, z{};
+    // std::cin >> x >> y;
 
+    // //if (x > y) z = x; else z = y;
+    // z = x > y ? x : y;
+    // std::cout << z << std::endl;
+
+    // int n{2};
+    // std::cout << 2 * n++ << std::endl;
+    // std::cout << n << std::endl;
+
+    // int m{2};
+    // std::cout << 2 * ++m << std::endl;
+    // std::cout << m << std::endl;
+
+    // int numOfStudents{ 30 };
+    // numOfStudents = 22;
+    // std::cout << numOfStudents << std::endl; //'l'ine
+    // std::cout << sizeof(numOfStudents) << std::endl;
+    // std::cout << typeid(numOfStudents).name() << std::endl;
+    // std::cout << typeid(static_cast<double>(numOfStudents)).name() << std::endl;
+
+    
     return 0;
 }
